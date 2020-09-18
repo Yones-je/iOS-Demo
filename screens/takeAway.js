ec99710fe6bd254c0d585acd54f7f24b15831655
Yones-je <yones.je@gmail.com>
2020-09-18 17:18:33 +0200

    Datepicker

--- screens/takeAway.js
+++ screens/takeAway.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const takeAway = ({navigation}) => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('datetime');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
   return (
     <View style={styles.main}>
-      <Text>När vill du hämta din mat?</Text>
+      <Text style={{marginTop: 20, fontSize: 20}}>
+        När vill du hämta din mat?
+      </Text>
       <Button
         color={'deepskyblue'}
-        title="Fortsätt"
+        title="Välj Datum"
+        onPress={showDatepicker}
+      />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      <Button
+        color={'deepskyblue'}
+        title="Fortsätt till menyn"
         onPress={() => {
           navigation.navigate('Menu');
         }}
